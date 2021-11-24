cdff32d724d1d57ff87ba5514d499774e25b6db1
Nate Hunter <hunter.j.nate@gmail.com>
2021-11-23 23:03:00 -0500

    user can get its history

--- server/src/routes/user.js
+++ server/src/routes/user.js
@@ -1,7 +1,7 @@
 import { PrismaClient } from "@prisma/client";
 import express from "express";
 import { protectRoute } from "../middleware/authorization";
-import { getVideoViews } from "./routeHelpers";
+import { getVideos } from "./routeHelpers";
 
 
 const prisma = new PrismaClient();
@@ -9,46 +9,21 @@ const prisma = new PrismaClient();
 function getUserRoutes() {
   const router = express.Router();
 
-  router.get('/liked-videos', protectRoute, getLikedVideos);
+  router.get('/liked-videos', protectRoute, getUserLikedVideos);
+  router.get('/history', protectRoute, getUserHistory);
 
   return router;
 }
 
-const getLikedVideos = async (req, res, next) => {
-  let likedVideos = await prisma.videoLike.findMany({
-    where: {
-      userId: {
-        equals: req.user.id,
-      },
-    },
-    include: {
-      video: true,
-    },
-    orderBy: {
-      createdAt: 'desc',
-    },
-  });
 
-  const likedVideoIds = likedVideos.map(video => video.videoId);
 
-  likedVideos = await prisma.video.findMany({
-    where: {
-      id: {
-        in: likedVideoIds,
-      },
-    },
-    include: {
-      user: true,
-    },
-  });
-
-  if (!likedVideos.length === 0) {
-    return res.status(200).json({ likedVideos });
-  }
-
-  likedVideos = await getVideoViews(likedVideos);
-
-  res.status(200).json({ likedVideos });
+const getUserLikedVideos = async (req, res, next) => {
+  await getVideos(prisma.videoLike, req, res);
 }
+
+const getUserHistory = async (req, res, next) => {
+  await getVideos(prisma.view, req, res);
+}
+
 
 export { getUserRoutes };
