037d284d3433851ceb4ded9f3521c30d61cb0128
Nate Hunter <hunter.j.nate@gmail.com>
2021-11-23 23:57:21 -0500

    user can toggle subscription to other users

--- server/src/routes/user.js
+++ server/src/routes/user.js
@@ -11,11 +11,10 @@ function getUserRoutes() {
 
   router.get('/liked-videos', protectRoute, getUserLikedVideos);
   router.get('/history', protectRoute, getUserHistory);
+  router.get('/:userId/subscribe', protectRoute, toggleSubscribe);
 
   return router;
 }
-
-
 
 const getUserLikedVideos = async (req, res, next) => {
   await getVideos(prisma.videoLike, req, res);
@@ -25,5 +24,65 @@ const getUserHistory = async (req, res, next) => {
   await getVideos(prisma.view, req, res);
 }
 
+const toggleSubscribe = async (req, res, next) => {
+  const currentUserId = req.user.id;
+  const subscribeUserId = req.params.userId;
+
+  if (currentUserId === subscribeUserId) {
+    return next({
+      message: 'Subscribing to your own channel is not allowed.',
+      statusCode: 400,
+    });
+  }
+
+  const userExists = await prisma.user.findUnique({
+    where: {
+      id: subscribeUserId,
+    },
+  });
+
+  if (!userExists) {
+    return next({
+      message: `User '${subscribeUserId}' does not exist.`,
+      statusCode: 404,
+    });
+  }
+
+  const isUserAlreadySubscribed = await prisma.subscription.findFirst({
+    where: {
+      subscriberId: {
+        equals: currentUserId,
+      },
+      subscribedToId: {
+        equals: subscribeUserId,
+      },
+    },
+  });
+
+  if (isUserAlreadySubscribed) {
+    await prisma.subscription.delete({
+      where: {
+        id: isUserAlreadySubscribed.id,
+      },
+    });
+  } else {
+    await prisma.subscription.create({
+      data: {
+        subscriber: {
+          connect: {
+            id: currentUserId,
+          },
+        },
+        subscribedTo: {
+          connect: {
+            id: subscribeUserId,
+          },
+        },
+      },
+    });
+  }
+
+  res.status(200).json({ message: 'Subscription handled.' })
+}
 
 export { getUserRoutes };
